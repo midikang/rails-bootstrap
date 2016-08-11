2552734a46c1478813d76a50cc09845705f9a1e8
Midi <midi13@163.com>
2016-08-11 03:43:39 +0000

    scaffold category, post

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
+  resources :categories
   root to: 'visitors#index'
 end
---
+++ spec/factories/posts.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :post do
+    category nil
+    title "MyString"
+    content "MyText"
+  end
+end
