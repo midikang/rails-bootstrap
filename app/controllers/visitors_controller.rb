2cc11f9d3b2a05b3f66034128b45e1f5d4ca2297
Midi <midi13@163.com>
2016-08-15 15:12:49 +0000

    show 3 most read posts on index

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,6 @@ class VisitorsController < ApplicationController
 
   def index
     @categories = Category.order(:name)
-    @posts = Post.order(updated_at: :desc).limit(3)
+    @posts = Post.order(visit_count: :desc).limit(3)
   end
 end
