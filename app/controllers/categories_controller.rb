fa6a7eea3f30ad368af9b94c7a6a0e8edfd53abb
Midi <midi13@163.com>
2016-09-07 02:56:37 +0000

    @categories = Category.order('name')

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,7 +5,7 @@ class CategoriesController < ApplicationController
   # GET /categories
   # GET /categories.json
   def index
-    @categories = Category.all
+    @categories = Category.order('name')
   end
 
   # GET /categories/1
